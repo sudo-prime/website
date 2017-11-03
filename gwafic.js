3f17265a0194146605843abdd56923f13286a8bd
sudo-prime <33303655+sudo-prime@users.noreply.github.com>
2017-11-03 12:50:07 -0400

    updated gwafic to use more efficient canvas clear function

--- gwafic.js
+++ gwafic.js
@@ -7,7 +7,7 @@ function draw() {
     var xPos = 2 * Math.cos(frameCount / 2);
     var yPos = 2 * Math.sin(frameCount / 2);
 
-    background('white');
+    clear();
 
     ellipse(50 + xPos, 50 + yPos, 80, 80);
 }
