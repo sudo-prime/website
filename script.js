a41b60a50fcbdf64f693a20965865e83dbff094a
Nick <nicholas.vandette@nuix.com>
2018-01-14 14:35:19 -0500

    bufix?

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+console.out(document)
+
 document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
 if (distance < 0) {
