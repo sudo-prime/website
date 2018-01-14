22f990fd71984ce48f2e58329e091465676f5c9b
Nick <nicholas.vandette@nuix.com>
2018-01-14 14:16:21 -0500

    making current time work

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+var countDownDate = new Date("Apr 6, 2018 0:00:00").getTime();
 
 var x = setInterval(function() {
 
   var now = new Date().getTime();
 
-  var distance = now;
+  var distance = countDownDate - now;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
