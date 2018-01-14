484624840bec7487fc9673b767c4bdb388c0c4b2
Nick <nicholas.vandette@nuix.com>
2018-01-14 14:55:32 -0500

    bufix?

--- script.js
+++ script.js
@@ -9,29 +9,29 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-console.log(document)
+window.onload = function() {
+	document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+	if (distance < 0) {
+	  document.getElementById("demo").innerHTML = "PAX IS UPON US!";
+	}
 
-if (distance < 0) {
-  document.getElementById("demo").innerHTML = "PAX IS UPON US!";
+	var timer = setInterval(function() {
+
+	  now = new Date().getTime();
+
+	  distance = countDownDate - now;
+
+	  days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+	  if (distance < 0) {
+	    clearInterval(timer);
+	    document.getElementById("demo").innerHTML = "PAX IS UPON US!";
+	  }
+	}, 1000);
 }
-
-var timer = setInterval(function() {
-
-  now = new Date().getTime(); 
-
-  distance = countDownDate - now;
-
-  days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById("demo").innerHTML = "PAX IS UPON US!";
-  }
-}, 1000);
