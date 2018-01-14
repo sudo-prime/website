92fabe19dc561295a6003464ebac8c588a5caed1
Nick <nicholas.vandette@nuix.com>
2018-01-14 14:25:47 -0500

    make it so timer displays immediately upon page load

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 var countDownDate = new Date("Apr 6, 2018 0:00:00").getTime();
 
-var x = setInterval(function() {
+var now = new Date().getTime();
 
-  var now = new Date().getTime();
+var distance = countDownDate - now;
 
-  var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+if (distance < 0) {
+  document.getElementById("demo").innerHTML = "PAX IS UPON US!";
+}
+
+var timer = setInterval(function() {
+
+  now = new Date().getTime();
+
+  distance = countDownDate - now;
+
+  days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   if (distance < 0) {
-    clearInterval(x);
+    clearInterval(timer);
     document.getElementById("demo").innerHTML = "PAX IS UPON US!";
   }
 }, 1000);
