21033e2dc3e1e43bcc45150925dd8fd8ec01fd41
Nick <nicholas.vandette@nuix.com>
2018-01-13 21:03:11 -0500

    making current time work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var timeStampInMs = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
 while(true) {
 	console.log(timeStampInMs, Date.now());
+	console.log("hello!ß")
 }
