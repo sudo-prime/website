10a280b7c30bfc71474c93871b30b049267d3cf8
Nicholas VanDette <nickvandettethree@gmail.com>
2018-07-14 22:39:04 -0400

    made the whole thing locally

--- home/script.js
+++ home/script.js
@@ -1,3 +1,42 @@
-//Change 829 to however many images are in the folder
-var rand_no = Math.floor(262*Math.random());
-document.images['RandomImg'].src="images/image_" + rand_no + ".jpg";
+const NUM_PICTURES = 262;
+const NUM_CATEGORIES = 4;
+
+document.images['RandomImg'].src = 'images/image_' + Math.floor(NUM_PICTURES * Math.random()) + '.jpg';
+
+
+// $("div.cell").map(function() {
+//   return
+// })
+
+function slideIn(selector, delay) {
+  selector.css('visibility','visible').hide().delay(delay).fadeIn(1000).animate({
+    'left': '0px'
+  }, {
+    duration: 1000,
+    queue: false
+  });
+}
+
+function get(table, x, y) {
+  return $($($($('table')[table]).children().children()[y]).find("div.cell")[x]);
+}
+
+function propogate(table, x, y, delay) {
+  slideIn(get(table, x, y), delay)
+  right = get(table, x + 1, y)
+  down = get(table, x, y + 1)
+  if (right.length !== 0) propogate(table, x + 1, y, delay+200);
+  if (down.length !== 0) propogate(table, x, y + 1, delay+200);
+}
+
+$(function() {
+  var headers = $('.container').children('h1')
+  slideIn($(headers[0]), 0)
+  propogate(0, 0, 0, 0);
+  slideIn($(headers[1]), 200)
+  propogate(1, 0, 0, 200);
+  slideIn($(headers[2]), 400)
+  propogate(2, 0, 0, 400);
+  slideIn($(headers[3]), 600)
+  propogate(3, 0, 0, 600);
+});
