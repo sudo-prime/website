837c8c1d93825c26ed2795bb030dbb0a0fc498fa
Nicholas VanDette <nickvandettethree@gmail.com>
2018-07-18 11:52:41 -0400

    added random welcome phrases, link changes

--- home/script.js
+++ home/script.js
@@ -1,7 +1,28 @@
 const NUM_PICTURES = 262;
 const NUM_CATEGORIES = 4;
 
+const USER_NAME = "Nick";
+const DINGUS_NAME = "Sam";
+
 document.images['RandomImg'].src = 'images/image_' + Math.floor(NUM_PICTURES * Math.random()) + '.jpg';
+
+var phrases = [
+  ['Sup, dude.', ''],
+  ['Good to see', 'you again!'],
+  ['Welcome back.', ''],
+  ['Welcome back.', ''],
+  ['Welcome back.', ''],
+  ['How\'s it going,', USER_NAME + '?'],
+  ['How\'s it going,', USER_NAME + '?'],
+  ['Hello again,', USER_NAME + '!'],
+  ['Hello again,', USER_NAME + '!'],
+  ['Hello again,', USER_NAME + '!'],
+  ['Hello again,', USER_NAME + '!'],
+  ['Yo, my guy.', ''],
+  ['Uh oh, is ' + DINGUS_NAME, ' watching...?'],
+  ['Hi, ' + USER_NAME + '!', ''],
+  ['Hi, ' + USER_NAME + '!', '']
+];
 
 function slideIn(selector, delay) {
   setTimeout((function() {
@@ -36,14 +57,9 @@ function slideIn3(selector, delay) {
   }), delay);
 }
 
-function get(table, x, y) {
-  return $($($($('table')[table]).children().children()[y]).find("div.cell")[x]);
-}
-
 function propogate(table, x, y, delay) {
   var right = get(table, x + 1, y);
   var down = get(table, x, y + 1);
-  if (table == 1) console.log(x, y, get(table, x + 1, y-1).length!==0);
   slideIn(get(table, x, y), delay);
   if (right.length !== 0 && get(table, x + 1, y-1).length === 0) propogate(table, x + 1, y, delay+200);
   if (down.length !== 0) propogate(table, x, y + 1, delay+200);
@@ -53,6 +69,9 @@ $(function() {
   var headers = $('.container').children('h1');
   var welcome1 = $('#welcome1');
   var welcome2 = $('#welcome2');
+  var phrase = phrases[Math.floor(Math.random() * phrases.length)]
+  $('#begin')[0].innerHTML = phrase[0];
+  $('#end')[0].innerHTML = phrase[1];
   slideIn2(welcome1, 0);
   slideIn($(headers[0]), 0);
   propogate(0, 0, 0, 0);
@@ -64,3 +83,51 @@ $(function() {
   slideIn($(headers[3]), 550);
   propogate(3, 0, 0, 600);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function get(table, x, y) {
+  return $($($($('table')[table]).children().children()[y]).find("div.cell")[x]);
+}
